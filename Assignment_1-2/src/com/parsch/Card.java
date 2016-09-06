b649933ad891dec705735732f27cf3e9e2def2e0
Anthony Parsch Jr <AnthonyParsch@gmail.com>
2016-09-06 10:12:20 -0700

    Added first programming assignment

---
+++ Assignment_1-2/src/com/parsch/Card.java
@@ -0,0 +1,6 @@
+package com.parsch;
+
+public enum Card 
+{
+	AceOfSpades, AceOfHearts, 
+}
---
+++ Assignment_1-2/src/com/parsch/Game.java
@@ -0,0 +1,19 @@
+package com.parsch;
+
+/*
+ * Menu:
+ * 	Hit - Get a new card
+ * 	Stand - Hold cards and let dealer draw
+ * 	Bust - Exceed 21 points
+ */
+
+public class Game 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
